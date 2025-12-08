36f5aef2038fe801f10b47b7d1139d9f53feea5c
Oleg Sorokin <fenix15@inbox.ru>
2025-12-08 14:13:46 +0300

    fix bot api

--- taskpulse-frontend/src/shared/config/env.ts
+++ taskpulse-frontend/src/shared/config/env.ts
@@ -4,4 +4,4 @@ export const API_BASE_URL =
   import.meta.env.VITE_API_BASE_URL || "http://pulse-zone.tech:8000";
 
 export const TELEGRAM_BOT_URL =
-  import.meta.env.VITE_TELEGRAM_BOT_URL || "https://t.me/Pulse_zone_bot";
+  import.meta.env.VITE_TELEGRAM_BOT_URL || "https://t.me/pulse_zone_tech_bot";
